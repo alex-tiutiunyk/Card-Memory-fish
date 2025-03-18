4d5a9a309decdb3738e0e2ed23f1e59d3d06d092
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-03-18 16:01:23 +0200

    add background to page and initial styles to form

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
+import styles from './styles.module.css';
 
 const Login = ({ onLogin }) => {
   const [formData, setFormData] = useState({ username: '', password: '' });
@@ -23,35 +24,41 @@ const Login = ({ onLogin }) => {
       }
     }
   };
-  
 
   const handleRegisterRedirect = () => {
     navigate('/register'); // Redirect to the Register page
   };
 
   return (
-    <form onSubmit={handleSubmit}>
-      <h2>Login</h2>
-      <input
-        type="text"
-        placeholder="Username"
-        value={formData.username}
-        onChange={(e) => setFormData({ ...formData, username: e.target.value })}
-      />
-      <input
-        type="password"
-        placeholder="Password"
-        value={formData.password}
-        onChange={(e) => setFormData({ ...formData, password: e.target.value })}
-      />
-      <div style={{ display: 'flex', gap: '10px' }}>
-        <button type="submit">Login</button>
-        <button type="button" onClick={handleRegisterRedirect}>
-          Register
-        </button>
-      </div>
-      <p style={{ color: 'red' }}>{error}</p>
-    </form>
+    <div className={styles.wrapper}>
+      <div className={styles.coral_1}></div>
+      <div className={styles.coral_2}></div>
+      <div className={styles.coral_3}></div>
+      <form onSubmit={handleSubmit} className={styles.form}>
+        <h2 className={styles.title}>Login</h2>
+        <input
+          className={styles.input}
+          type='text'
+          placeholder='Username'
+          value={formData.username}
+          onChange={(e) => setFormData({ ...formData, username: e.target.value })}
+        />
+        <input
+          className={styles.input}
+          type='password'
+          placeholder='Password'
+          value={formData.password}
+          onChange={(e) => setFormData({ ...formData, password: e.target.value })}
+        />
+        <div style={{ display: 'flex', gap: '10px' }}>
+          <button type='submit'>Login</button>
+          <button type='button' onClick={handleRegisterRedirect}>
+            Register
+          </button>
+        </div>
+        <p style={{ color: 'red' }}>{error}</p>
+      </form>
+    </div>
   );
 };
 
