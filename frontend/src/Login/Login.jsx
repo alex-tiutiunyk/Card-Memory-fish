5bce4c62a830f007865e3edfb1480776e65f3122
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-03-18 22:19:17 +0200

    add changes to Register page

--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,52 +1,13 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import axios from 'axios';
-import { generateBubbles } from '../utils/bubbleGenerator';
-import { generateFish } from '../utils/fishGenerator';
 import styles from './styles.module.css';
+import Aquarium from '../components/Aquarium/Aquarium';
 
 const Login = ({ onLogin }) => {
   const [formData, setFormData] = useState({ username: '', password: '' });
   const [error, setError] = useState('');
   const navigate = useNavigate(); // For navigation
-
-  useEffect(() => {
-    const interval = setInterval(generateBubbles, 500);
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    const interval = setInterval(generateFish, 1500);
-    return () => clearInterval(interval);
-  }, []);
-
-  useEffect(() => {
-    let event = '';
-    const isTouchSupported =
-      'ontouchstart' in window || navigator.maxTouchPoints > 0 || navigator.msMaxTouchPoints > 0;
-
-    isTouchSupported ? (event = 'touchmove') : (event = 'mousemove');
-
-    const onMove = (e) => {
-      let clientEvent = e;
-
-      if (isTouchSupported) {
-        const touch = e.touches[0];
-        clientEvent = touch;
-      }
-
-      Object.assign(document.documentElement, {
-        style: `
-        --move-x: ${((clientEvent.clientX - window.innerWidth / 2) * -0.007).toFixed(1)}deg;
-        --move-y: ${((clientEvent.clientY - window.innerHeight / 2) * 0.015).toFixed(1)}deg;
-      `,
-      });
-    };
-
-    document.addEventListener(event, onMove);
-
-    return () => document.removeEventListener(event, onMove);
-  }, []);
 
   const handleSubmit = async (e) => {
     e.preventDefault();
@@ -71,12 +32,7 @@ const Login = ({ onLogin }) => {
 
   return (
     <div className={styles.wrapper}>
-      <div className={`aquarium ${styles.container}`}>
-        <div className={styles.layer}></div>
-        <div className={styles.coral_1}></div>
-        <div className={styles.coral_2}></div>
-        <div className={styles.coral_3}></div>
-      </div>
+      <Aquarium />
       <form onSubmit={handleSubmit} className={styles.form}>
         <h2 className={styles.title}>Login</h2>
         <input
