f58a7cb76dba076c6c1c9249afc8f32d321886a8
Alex Tiutiunyk <alex.tiutiunyk@gmail.com>
2025-03-19 09:52:40 +0200

    add new bg and animation to Play page

--- frontend/src/MemoryCardGame/Play.jsx
+++ frontend/src/MemoryCardGame/Play.jsx
@@ -1,65 +1,65 @@
-import React, { useState, useRef, useEffect } from "react";
-import { useNavigate } from "react-router-dom";
-import Modal from "react-modal";
-import backgroundGif from "../assets/images/play.gif";
-import calmBackground from "../assets/images/calm-wallpaper.jpg";
-import backgroundMusic from "../assets/audio/background-music.mp3";
-import buttonHoverSound from "../assets/audio/button-hover.mp3";
-import buttonClickSound from "../assets/audio/button-click.mp3";
-import { X } from "lucide-react";
-import "./Play.css";
+import { useState, useRef, useEffect } from 'react';
+import { useNavigate } from 'react-router-dom';
+import Modal from 'react-modal';
+import backgroundGif from '../../public/images/bg-layer-2.jpg';
+import calmBackground from '../assets/images/calm-wallpaper.jpg';
+import backgroundMusic from '../assets/audio/background-music.mp3';
+import buttonHoverSound from '../assets/audio/button-hover.mp3';
+import buttonClickSound from '../assets/audio/button-click.mp3';
+import { X } from 'lucide-react';
+import './Play.css';
 
 const modalStyles = {
   overlay: {
-    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
     zIndex: 999,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    overflow: "hidden",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
   },
   content: {
-    backgroundColor: "#1e1e2e",
-    border: "2px solid #4a4e69",
-    borderRadius: "20px",
-    padding: "40px",
-    maxWidth: "600px",
-    height: "300px",
-    width: "90%",
-    color: "#fff",
-    textAlign: "center",
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    overflow: "hidden",
+    backgroundColor: '#1e1e2e',
+    border: '2px solid #4a4e69',
+    borderRadius: '20px',
+    padding: '40px',
+    maxWidth: '600px',
+    height: '300px',
+    width: '90%',
+    color: '#fff',
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    overflow: 'hidden',
   },
 };
 
 const modalPlayStyles = {
   overlay: {
-    backgroundColor: "rgba(0, 0, 0, 0.7)",
+    backgroundColor: 'rgba(0, 0, 0, 0.7)',
     zIndex: 999,
-    display: "flex",
-    alignItems: "center",
-    justifyContent: "center",
-    overflow: "hidden",
+    display: 'flex',
+    alignItems: 'center',
+    justifyContent: 'center',
+    overflow: 'hidden',
   },
   content: {
-    backgroundColor: "#1e1e2e",
-    border: "2px solid #4a4e69",
-    borderRadius: "20px",
-    padding: "40px",
-    maxWidth: "600px",
-    height: "200px",
-    width: "90%",
-    color: "#fff",
-    textAlign: "center",
-    position: "absolute",
-    top: "50%",
-    left: "50%",
-    transform: "translate(-50%, -50%)",
-    overflow: "hidden",
+    backgroundColor: '#1e1e2e',
+    border: '2px solid #4a4e69',
+    borderRadius: '20px',
+    padding: '40px',
+    maxWidth: '600px',
+    height: '200px',
+    width: '90%',
+    color: '#fff',
+    textAlign: 'center',
+    position: 'absolute',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    overflow: 'hidden',
   },
 };
 
@@ -69,12 +69,14 @@ const Play = () => {
   const [PlaymodalIsOpen, setModalPlayIsOpen] = useState(false);
   const [difficulty, setDifficulty] = useState(null);
   const [isCalmMode, setIsCalmMode] = useState(false);
-  
+
   const [bgVolume, setBgVolume] = useState(
-    localStorage.getItem("bgVolume") !== null ? parseInt(localStorage.getItem("bgVolume"), 10) : 50
+    localStorage.getItem('bgVolume') !== null ? parseInt(localStorage.getItem('bgVolume'), 10) : 50,
   );
   const [sfxVolume, setSfxVolume] = useState(
-    localStorage.getItem("sfxVolume") !== null ? parseInt(localStorage.getItem("sfxVolume"), 10) : 50
+    localStorage.getItem('sfxVolume') !== null
+      ? parseInt(localStorage.getItem('sfxVolume'), 10)
+      : 50,
   );
 
   const [mutedBg, setMutedBg] = useState(false);
@@ -94,13 +96,13 @@ const Play = () => {
     bgAudio.volume = bgVolume / 100;
 
     const startMusic = () => {
-      bgAudio.play().catch((error) => console.error("Autoplay failed:", error));
+      bgAudio.play().catch((error) => console.error('Autoplay failed:', error));
     };
 
-    document.addEventListener("click", startMusic, { once: true });
+    document.addEventListener('click', startMusic, { once: true });
 
     return () => {
-      document.removeEventListener("click", startMusic);
+      document.removeEventListener('click', startMusic);
       bgAudio.pause();
       bgAudio.currentTime = 0;
     };
@@ -110,13 +112,13 @@ const Play = () => {
     if (bgAudioRef.current) {
       bgAudioRef.current.volume = bgVolume / 100;
     }
-    localStorage.setItem("bgVolume", bgVolume);
+    localStorage.setItem('bgVolume', bgVolume);
   }, [bgVolume]);
 
   useEffect(() => {
     hoverAudioRef.current.volume = sfxVolume / 100;
     clickAudioRef.current.volume = sfxVolume / 100;
-    localStorage.setItem("sfxVolume", sfxVolume);
+    localStorage.setItem('sfxVolume', sfxVolume);
   }, [sfxVolume]);
 
   const handleBgVolumeChange = (event) => {
@@ -138,16 +140,16 @@ const Play = () => {
 
   const playHoverSound = () => {
     hoverAudioRef.current.currentTime = 0;
-    hoverAudioRef.current.play().catch((error) =>
-      console.error("Hover sound playback failed:", error)
-    );
+    hoverAudioRef.current
+      .play()
+      .catch((error) => console.error('Hover sound playback failed:', error));
   };
 
   const playClickSound = () => {
     clickAudioRef.current.currentTime = 0;
-    clickAudioRef.current.play().catch((error) =>
-      console.error("Click sound playback failed:", error)
-    );
+    clickAudioRef.current
+      .play()
+      .catch((error) => console.error('Click sound playback failed:', error));
   };
 
   const SettingopenModal = () => {
@@ -176,30 +178,30 @@ const Play = () => {
 
   const handlePlay = () => {
     playClickSound();
-    const userID = localStorage.getItem("userID");
+    const userID = localStorage.getItem('userID');
     if (!userID) {
-      alert("UserID is missing. Please log in again.");
+      alert('UserID is missing. Please log in again.');
       return;
     }
-    localStorage.setItem("gameStarted", "true");
+    localStorage.setItem('gameStarted', 'true');
 
     if (isCalmMode) {
-      if (difficulty === "red") {
-        navigate("/calm-hard");
-      } else if (difficulty === "yellow") {
-        navigate("/calm-medium");
-      } else if (difficulty === "green") {
-        navigate("/calm-easy");
+      if (difficulty === 'red') {
+        navigate('/calm-hard');
+      } else if (difficulty === 'yellow') {
+        navigate('/calm-medium');
+      } else if (difficulty === 'green') {
+        navigate('/calm-easy');
       } else {
         alert(`Selected difficulty: ${difficulty}`);
       }
     } else {
-      if (difficulty === "red") {
-        navigate("/memory-card-game");
-      } else if (difficulty === "yellow") {
-        navigate("/medium");
-      } else if (difficulty === "green") {
-        navigate("/easy");
+      if (difficulty === 'red') {
+        navigate('/memory-card-game');
+      } else if (difficulty === 'yellow') {
+        navigate('/medium');
+      } else if (difficulty === 'green') {
+        navigate('/easy');
       } else {
         alert(`Selected difficulty: ${difficulty}`);
       }
@@ -208,35 +210,33 @@ const Play = () => {
 
   return (
     <div
-      className="background-container"
+      className='background-container'
       style={{
         backgroundImage: `url(${isCalmMode ? calmBackground : backgroundGif})`,
       }}
     >
-      <h1 className={`game-title ${isCalmMode ? "calm-title" : ""}`}>
-        WonderCards
-      </h1>
-
-      <div className="button-container">
+      <div className='bathyscaphe'></div>
+      <h1 className={`game-title ${isCalmMode ? 'calm-title' : ''}`}>WonderCards</h1>
+      <div className='button-container'>
         <button
-          className={`game-button ${isCalmMode ? "calm-button" : ""}`}
+          className={`game-button ${isCalmMode ? 'calm-button' : ''}`}
           onClick={PlayopenModal}
           onMouseEnter={playHoverSound}
         >
           Play
         </button>
         <button
-          className={`game-button ${isCalmMode ? "calm-button" : ""}`}
+          className={`game-button ${isCalmMode ? 'calm-button' : ''}`}
           onClick={() => {
             playClickSound();
-            alert("Instructions coming soon!");
+            alert('Instructions coming soon!');
           }}
           onMouseEnter={playHoverSound}
         >
           Instructions
         </button>
         <button
-          className={`game-button ${isCalmMode ? "calm-button" : ""}`}
+          className={`game-button ${isCalmMode ? 'calm-button' : ''}`}
           onClick={SettingopenModal}
           onMouseEnter={playHoverSound}
         >
@@ -250,53 +250,49 @@ const Play = () => {
           ...modalStyles,
           content: {
             ...modalStyles.content,
-            backgroundColor: isCalmMode ? "#86a17d" : "#1e1e2e",
-            color: isCalmMode ? "#ffffff" : "#fff",
+            backgroundColor: isCalmMode ? '#86a17d' : '#1e1e2e',
+            color: isCalmMode ? '#ffffff' : '#fff',
           },
         }}
       >
         <button
           onClick={SettingcloseModal}
           style={{
-            position: "absolute",
-            top: "10px",
-            right: "10px",
-            background: "none",
-            border: "none",
-            cursor: "pointer",
-            color: "#fff",
+            position: 'absolute',
+            top: '10px',
+            right: '10px',
+            background: 'none',
+            border: 'none',
+            cursor: 'pointer',
+            color: '#fff',
           }}
         >
           <X size={24} />
         </button>
 
-        <h2 className={`${isCalmMode ? "calm-mode-label" : ""} modal-h2`}>
-          Background Music
-        </h2>
-        <div className="volume-control">
-          <span className="volume-icon">{mutedBg ? "🔇" : "🔊"}</span>
+        <h2 className={`${isCalmMode ? 'calm-mode-label' : ''} modal-h2`}>Background Music</h2>
+        <div className='volume-control'>
+          <span className='volume-icon'>{mutedBg ? '🔇' : '🔊'}</span>
           <input
-            type="range"
-            min="0"
-            max="100"
+            type='range'
+            min='0'
+            max='100'
             value={bgVolume}
             onChange={handleBgVolumeChange}
-            className="volume-slider"
+            className='volume-slider'
           />
         </div>
 
-        <h2 className={`${isCalmMode ? "calm-mode-label" : ""} modal-h2`}>
-          Sound Effects
-        </h2>
-        <div className="volume-control">
-          <span className="volume-icon">{mutedSfx ? "🔇" : "🔊"}</span>
+        <h2 className={`${isCalmMode ? 'calm-mode-label' : ''} modal-h2`}>Sound Effects</h2>
+        <div className='volume-control'>
+          <span className='volume-icon'>{mutedSfx ? '🔇' : '🔊'}</span>
           <input
-            type="range"
-            min="0"
-            max="100"
+            type='range'
+            min='0'
+            max='100'
             value={sfxVolume}
             onChange={handleSfxVolumeChange}
-            className="volume-slider"
+            className='volume-slider'
           />
         </div>
 
@@ -314,7 +310,6 @@ const Play = () => {
           </label>
         </div> */}
       </Modal>
-
       <Modal
         isOpen={PlaymodalIsOpen}
         onRequestClose={PlaycloseModal}
@@ -322,62 +317,60 @@ const Play = () => {
           ...modalPlayStyles,
           content: {
             ...modalPlayStyles.content,
-            backgroundColor: isCalmMode ? "#86a17d" : "#1e1e2e",
-            color: isCalmMode ? "#ffffff" : "#fff",
+            backgroundColor: isCalmMode ? '#86a17d' : '#1e1e2e',
+            color: isCalmMode ? '#ffffff' : '#fff',
           },
         }}
       >
         <button
           onClick={PlaycloseModal}
           style={{
-            position: "absolute",
-            top: "10px",
-            right: "10px",
-            background: "none",
-            border: "none",
-            cursor: "pointer",
-            color: "#fff",
+            position: 'absolute',
+            top: '10px',
+            right: '10px',
+            background: 'none',
+            border: 'none',
+            cursor: 'pointer',
+            color: '#fff',
           }}
         >
           <X size={24} />
         </button>
 
-        <h2 className={`${isCalmMode ? "calm-mode-label" : ""} modal-h2`}>
-          Select Difficulty
-        </h2>
-        <div className="difficulty-selection">
+        <h2 className={`${isCalmMode ? 'calm-mode-label' : ''} modal-h2`}>Select Difficulty</h2>
+        <div className='difficulty-selection'>
           <button
             onClick={() => {
-              handleDifficultySelect("green");
+              handleDifficultySelect('green');
               playClickSound();
             }}
             className={`difficulty-button green ${
-              difficulty === "green" && !isCalmMode ? "selected" : ""
-            } ${isCalmMode && difficulty === "green" ? "calm-selected" : ""}`}
+              difficulty === 'green' && !isCalmMode ? 'selected' : ''
+            } ${isCalmMode && difficulty === 'green' ? 'calm-selected' : ''}`}
             onMouseEnter={playHoverSound}
           >
             Easy
           </button>
           <button
             onClick={() => {
-              handleDifficultySelect("yellow");
+              handleDifficultySelect('yellow');
               playClickSound();
             }}
             className={`difficulty-button yellow ${
-              difficulty === "yellow" && !isCalmMode ? "selected" : ""
-            } ${isCalmMode && difficulty === "yellow" ? "calm-selected" : ""}`}
+              difficulty === 'yellow' && !isCalmMode ? 'selected' : ''
+            } ${isCalmMode && difficulty === 'yellow' ? 'calm-selected' : ''}`}
             onMouseEnter={playHoverSound}
           >
             Normal
           </button>
           <button
             onClick={() => {
-              handleDifficultySelect("red");
+              handleDifficultySelect('red');
               playClickSound();
             }}
             className={`difficulty-button red ${
-              difficulty === "red" && !isCalmMode ? "selected" : ""
-            } ${isCalmMode && difficulty === "red" ? "calm-selected" : ""}`}
+              difficulty === 'red' && !isCalmMode ? 'selected' : ''
+            } ${isCalmMode && difficulty === 'red' ? 'calm-selected' : ''}`}
             onMouseEnter={playHoverSound}
           >
             Hard
@@ -385,11 +378,7 @@ const Play = () => {
         </div>
 
         <div>
-          <button
-            onClick={handlePlay}
-            className="play-button"
-            onMouseEnter={playHoverSound}
-          >
+          <button onClick={handlePlay} className='play-button' onMouseEnter={playHoverSound}>
             Accept
           </button>
         </div>
